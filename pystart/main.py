9b4e3bdba3ff4cb6468325fa4394768ada45e9c9
Andrii <dikoua9917@gmail.com>
2026-06-17 10:51:27 +0300

    feat:directory creation and validation

--- pystart/main.py
+++ pystart/main.py
@@ -1,6 +1,7 @@
+from pathlib import Path
+
 import typer
 
-# Initialize the Typer application
 app = typer.Typer()
 
 
@@ -9,7 +10,26 @@ def create(project_name: str):
     """
     Scaffold a new Python project.
     """
-    print(f"Creating a new project named: {project_name}")
+    # Resolve the path where the project should be created
+    project_path = Path.cwd() / project_name
+
+    #  Path Validation
+    if project_path.exists():
+        typer.echo(
+            f"Error: A directory or file named '{project_name}' already exists here.",
+            err=True,
+        )
+        raise typer.Exit(code=1)
+
+    # Directory Creation
+    try:
+        project_path.mkdir(parents=True, exist_ok=False)
+        typer.echo(f"Successfully created directory: {project_path}")
+    except Exception as e:
+        typer.echo(
+            f"An unexpected error occurred while creating the directory: {e}", err=True
+        )
+        raise typer.Exit(code=1)
 
 
 if __name__ == "__main__":
